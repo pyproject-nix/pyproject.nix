6efae2d58fe3f442ce7ad86a73b711180c3597a2
con-f-use <con-f-use@users.noreply.github.com>
2026-03-24 16:00:02 +0100

    templates: streamline `system`

--- templates/impure/flake.nix
+++ templates/impure/flake.nix
@@ -7,36 +7,26 @@
 
   outputs =
     { nixpkgs, ... }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-    in
     {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.python3
-              pkgs.uv
-            ];
+      devShells = builtins.mapAttrs (system: pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.python3
+            pkgs.uv
+          ];
 
-            env = lib.optionalAttrs pkgs.stdenv.isLinux {
-              # Python libraries often load native shared objects using dlopen(3).
-              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-            };
-
-            shellHook = ''
-              unset PYTHONPATH
-              uv sync
-              . .venv/bin/activate
-            '';
+          env = pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+            # Python libraries often load native shared objects using dlopen(3).
+            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
           };
-        }
-      );
+
+          shellHook = ''
+            unset PYTHONPATH
+            uv sync
+            source .venv/bin/activate
+          '';
+        };
+      }) nixpkgs.legacyPackages;
     };
 }
