946e5fb82c3443940c45e5a61686d173e2e21155
adisbladis <adisbladis@gmail.com>
2026-04-03 19:28:21 +1300

    build.hooks.build-editable: Add tomli to Python <3.11
    It's a bit hacky and uses nixpkgs Python package definitions, but at least it's better than a broken package.

--- build/editable/default.nix
+++ build/editable/default.nix
@@ -1,8 +1,20 @@
 {
   stdenv,
   python3,
+  python3Packages,
   lib,
 }:
+let
+  # If Python < 3.11 it doesn't have builtin TOML support
+  # Hack it in by using the nixpkgs tomli package
+  toml-hack = if python3.pythonOlder "3.11" then ''
+    cat >> $out/bin/build-editable <<EOF
+    import sys
+    sys.path.append('${python3Packages.tomli}/${python3Packages.python.sitePackages}')
+    EOF
+  '' else "";
+
+in
 stdenv.mkDerivation {
   name = "build-editable";
 
@@ -14,6 +26,7 @@ stdenv.mkDerivation {
     runHook preInstall
     mkdir -p $out/bin
     echo '#!${python3.interpreter}' > $out/bin/build-editable
+    ${toml-hack}
     cat ${./src/build_editable/__init__.py} >> $out/bin/build-editable
     chmod +x $out/bin/build-editable
     runHook postInstall
